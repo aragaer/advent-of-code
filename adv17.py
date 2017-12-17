b20f2e66763ea730471e7165987a9353359a87c6
aragaer <aragaer@gmail.com>
2017-12-17 08:23:35 +0300

    Don't need no functions

--- adv17.py
+++ adv17.py
@@ -8,32 +8,23 @@ if False:
 else:
     steps = 354
 
-def do_iteration(value):
-    global pos
+for i in range(1, 2017+1):
     pos += steps
     pos %= len(buf)
-    buf.insert(pos+1, value)
+    buf.insert(pos+1, i)
     pos += 1
 
-for i in range(1, 2017+1):
-    do_iteration(i)
-
-print(buf[pos-1], buf[pos], buf[pos+1])
+print(buf[pos+1])
 
 after_0 = buf[1]
-print(after_0)
-
 l = len(buf)
-def do_simpler(value):
-    global l, pos, after_0
+
+for i in range(2017+1, 50000000+1):
     pos += steps
     pos %= l
     l += 1
     if pos == 0:
-        after_0 = value
+        after_0 = i
     pos += 1
-
-for i in range(2017+1, 50000000+1):
-    do_simpler(i)
 
 print(after_0)
