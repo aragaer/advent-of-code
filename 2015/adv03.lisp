a772af9f46d6e45ed3740c85f78c77276e13ae5c
aragaer <aragaer@gmail.com>
2017-12-28 11:37:48 +0300

    2015 Day 03. Solution in common lisp.

---
+++ 2015/adv03.lisp
@@ -0,0 +1,20 @@
+(defmacro move (x y)
+  `(cond ((eql c #\<) (decf ,x))
+	 ((eql c #\>) (incf ,x))
+	 ((eql c #\^) (incf ,y))
+	 ((eql c #\v) (decf ,y))))
+
+(defmacro visit (x y l)
+  `(setf (gethash (cons ,x ,y) ,l) t))
+
+(loop for c across (read-line *standard-input*)
+      with visited1 = (make-hash-table :test 'equal)
+      with visited2 = (make-hash-table :test 'equal)
+      with x = 0 and y = 0
+      with x2 = 0 and y2 = 0 and ox = 0 and oy = 0
+      initially (visit 0 0 visited1) (visit 0 0 visited2)
+      do (move x y) (visit x y visited1)
+      do (rotatef x2 ox) (rotatef y2 oy)
+      do (move x2 y2) (visit x2 y2 visited2)
+      finally (format t "Part 1: ~d~%" (hash-table-count visited1))
+      finally (format t "Part 2: ~d~%" (hash-table-count visited2)))
