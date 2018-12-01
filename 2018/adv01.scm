1539146de086c0dab1ce7faa50768d92fd47eed6
aragaer <aragaer@gmail.com>
2018-12-01 12:51:55 +0300

    2018.01 - moar functions

--- 2018/adv01.scm
+++ 2018/adv01.scm
@@ -3,24 +3,31 @@
 (define data (list (read)))
 (define tail data)
 
+(define advance-tail
+  (lambda (tail value)
+    (set-cdr! tail (list value))
+    (cdr tail)))
+
 (do ((value (read) (read)))
     ((eof-object? value))
-  (set-cdr! tail (list value))
-  (set! tail (cdr tail)))
+  (set! tail (advance-tail tail value)))
 
 (printf "~a\n" (foldl + 0 data))
 
 (set-cdr! tail data)
 
-(define seen (make-hash-table))
+(define seen (alist->hash-table '((0 . #t))))
+(define seen?
+  (lambda (freq)
+    (if (hash-table-exists? seen freq)
+        (abort freq)
+        (hash-table-set! seen freq #t))))
+
+(define register
+  (lambda (freq value)
+    (seen? (+ freq value))
+    (+ freq value)))
 
 (handle-exceptions exn
                    (printf "~a\n" exn)
-                   (foldl
-                    (lambda (freq value)
-                      (hash-table-set! seen freq #t)
-                      (set! freq (+ freq value))
-                      (if (hash-table-exists? seen freq)
-                          (abort freq)
-                          freq))
-                    0 data))
+                   (foldl register 0 data))
