b4d1244f69064b6c8cb3f698f1d04a1c71c9958d
aragaer <aragaer@gmail.com>
2018-12-02 13:42:57 +0300

    2018.02: Rewrite with rassoc and hash-table-update

--- 2018/adv02.scm
+++ 2018/adv02.scm
@@ -1,26 +1,22 @@
-(require-extension srfi-69)  ; hash-table
 (require-extension srfi-13)  ; string->list
+(require-extension srfi-69)  ; hash-table
 (use loop)
 
 (define ids '())
 
-(define has-num?
-  (lambda (num table)
-    (foldl (lambda (f c) (or f (= num c))) #f (hash-table-values table))))
-
 (define count-chars
   (lambda (word)
-    (loop for char in (string->list word)
-          with table = (make-hash-table)
-          do (hash-table-set! table char (+ 1 (hash-table-ref/default table char 0)))
-          finally (return table))))
+    (let* ((table (make-hash-table))
+           (count (lambda (c) (hash-table-update!/default table c add1 0))))
+      (for-each count (string->list word))
+      (hash-table->alist table))))
 
 (loop for line = (read-line)
       until (eof-object? line)
       for table = (count-chars line)
-      collect line into lines
-      count (has-num? 2 table) into twos
-      count (has-num? 3 table) into threes
+      collect (string->list line) into lines
+      count (rassoc 2 table) into twos
+      count (rassoc 3 table) into threes
       finally (print (* twos threes))
       finally (set! ids lines))
 
@@ -28,8 +24,8 @@
 
 (define levenstein
   (lambda (w1 w2)
-    (loop for c1 in (string->list w1)
-          for c2 in (string->list w2)
+    (loop for c1 in w1
+          for c2 in w2
           when (equal? c1 c2)
             collect c1 into same
           else
@@ -46,4 +42,7 @@
       for word2 = (car res)
       for lev = (cdr res)
       until (= 1 lev)
-      finally (printf "~a ~a ~a ~a\n" word word2 lev common))
+      finally (printf "~a ~a ~a ~a\n"
+                      (list->string word)
+                      (list->string word2)
+                      lev common))
