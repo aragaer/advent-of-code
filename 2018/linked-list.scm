af2d48cccfe68cdaf36963fc9d34e3b6ec6e5b0c
aragaer <aragaer@gmail.com>
2018-12-09 20:35:19 +0300

    2018.09: Extract linked list stuff to a separate file

---
+++ 2018/linked-list.scm
@@ -0,0 +1,19 @@
+(define (linked-list-insert-after item new-item)
+  (linked-list-link new-item (linked-list-next item))
+  (linked-list-link item new-item))
+
+(define (linked-list-remove item)
+  (let ((next (linked-list-next item))
+        (prev (linked-list-prev item)))
+    (linked-list-link prev next)))
+
+(define (linked-list-link item1 item2)
+  (set-car! (cdr item1) item2)
+  (set-cdr! (cdr item2) item1))
+
+(define linked-list-next cadr)
+(define linked-list-prev cddr)
+(define linked-list-value car)
+
+(define (make-linked-list-item value)
+  (list value '()))
