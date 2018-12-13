be352ea17e0ec4ed1a165b22b852fab3a5fd0169
aragaer <aragaer@gmail.com>
2018-12-13 11:55:21 +0300

    2018.13: Readability

--- 2018/adv13.scm
+++ 2018/adv13.scm
@@ -1,11 +1,6 @@
 (load "common.scm")
 (define *debug* #f)
 
-(define direction-up +i)
-(define direction-down -i)
-(define direction-left -1)
-(define direction-right 1)
-
 (define (turn-up-right direction)
   (+ (imag-part direction) (* +i (real-part direction))))
 
@@ -14,23 +9,32 @@
 
 (define (turn-left direction) (* +i direction))
 (define (turn-right direction) (* -i direction))
-(define turn-keep identity)
+(define turn-straight identity)
 
-(define ((turn char) direction next-turn)
+(define position-of first)
+(define direction-of second)
+
+(define (char->direction char)
+  (if* (assoc char '((#\v . -i) (#\^ . +i) (#\< . -1) (#\> . +1)))
+       (cdr it)
+       #f))
+
+(define (turn-func char)
   (case char
-    ((#\-) (values direction next-turn))
-    ((#\|) (values direction next-turn))
-    ((#\\) (values (turn-up-left direction) next-turn))
-    ((#\/) (values (turn-up-right direction) next-turn))
-    ((#\+) (values ((car next-turn) direction) (cdr next-turn)))))
+    ((#\\) (lambda (cart) (set! (direction-of cart) (turn-up-left (direction-of cart)))))
+    ((#\/) (lambda (cart) (set! (direction-of cart) (turn-up-right (direction-of cart)))))
+    ((#\+) (lambda (cart) (let ((next-turn (third cart)))
+                            (set-cdr! cart (list ((car next-turn) (direction-of cart))
+                                                 (cdr next-turn))))))
+    (else identity)))
 
 (define (make-cart position direction)
-  (list position direction (circular-list turn-left turn-keep turn-right)))
+  (list position direction (circular-list turn-left turn-straight turn-right)))
 
 (when *debug*
     (define (debug-turn direction char)
       (format #t "From direction ~a at char ~a turn to direction ~a~%"
-              direction char ((turn char) direction #f)))
+              direction char ((turn-func char) direction #f)))
     (debug-turn +i #\\))
 
 (define carts '())
@@ -41,48 +45,34 @@
          for line = (read-line)
          until (eof-object? line)
          append (map (lambda (x char)
-                        (let ((position (+ x y)))
-                          (cons position
-                                (case char
-                                  ((#\v)
-                                   (push! (make-cart position direction-down) carts)
-                                   (turn #\|))
-                                  ((#\^)
-                                   (push! (make-cart position direction-up) carts)
-                                   (turn #\|))
-                                  ((#\<)
-                                   (push! (make-cart position direction-left) carts)
-                                   (turn #\-))
-                                  ((#\>)
-                                   (push! (make-cart position direction-right) carts)
-                                   (turn #\-))
-                                  (else (turn char))))))
+                       (let ((position (+ x y)))
+                         (if* (char->direction char)
+                              (push! (make-cart position it) carts))
+                         (cons position (turn-func char))))
                       (range 0 (string-length line)) (string->list line)))))
 
-(define (get-track-at position)
+(define (track-func-at position)
   (hash-table-ref tracks position))
 
 (define (pos->string pos)
   (format #f "~a,~a" (real-part pos) (- (imag-part pos))))
 
 (define (collision-handler cart1 cart2)
-  (format #t "Collision at ~a~%" (pos->string (first cart1)))
+  (format #t "Collision at ~a~%" (pos->string (position-of cart1)))
   (set! carts (delete cart1 (delete cart2 carts))))
 
 (define (move cart)
-  (let* ((pos (first cart))
-         (new-pos (+ pos (second cart))))
+  (let* ((pos (position-of cart))
+         (new-pos (+ pos (direction-of cart))))
     (if *debug*
         (printf "Cart from ~a moved to ~a~%" (pos->string pos) (pos->string new-pos)))
     (when (member cart carts)
       (set-car! cart new-pos)
       (if* (find (lambda (other)
-                   (= new-pos (first other)))
+                   (= new-pos (position-of other)))
                  (delete cart carts))
-          (collision-handler cart it))
-      (let-values (((direction next-turn)
-                    ((get-track-at new-pos) (second cart) (third cart))))
-        (set-cdr! cart (list direction next-turn))))))
+           (collision-handler cart it))
+      ((track-func-at new-pos) cart))))
 
 (define (complex-cmp n1 n2)
   (let ((x1 (real-part n1))
@@ -95,7 +85,8 @@
 
 (loop do (for-each move (sort carts
                               (lambda (cart1 cart2)
-                                (complex-cmp (first cart1) (first cart2)))))
+                                (complex-cmp (position-of cart1) (position-of cart2)))))
       while (< 1 (length carts))
-      finally (let ((pos (caar carts)))
-                (printf "Last cart at ~a\n" (pos->string pos))))
+      finally (if (pair? carts)
+                  (printf "Last cart at ~a\n" (pos->string (caar carts)))
+                  (print "No carts left")))
