24b77d2e42f8e46273b49dec819f4442e3ee4c53
aragaer <aragaer@gmail.com>
2019-12-01 17:01:39 +0300

    Update 2019-12-01

--- 2019/day1/src/main.rs
+++ 2019/day1/src/main.rs
@@ -2,32 +2,30 @@ use std::cmp;
 use std::env;
 use std::fs;
 use std::io;
+use std::io::BufRead;
+use std::os::unix::io::FromRawFd;
 
 fn fuel_for(mass: i32) -> i32 {
     cmp::max(mass / 3 - 2, 0)
 }
 
-fn read_input() -> String {
-    let mut contents = String::new();
-    match env::args().nth(1) {
-        None => Box::new(io::stdin()) as Box<dyn io::Read>,
-        Some(filename) => Box::new(fs::File::open(filename)
-                                   .expect("Failed to open file")),
-    }.read_to_string(&mut contents)
-        .expect("Failed to read");
-
-    contents
+fn input() -> impl Iterator<Item=io::Result<String>> {
+    let reader = io::BufReader::new(match env::args().nth(1) {
+        None => unsafe {fs::File::from_raw_fd(0)},
+        Some(filename) => fs::File::open(filename).unwrap(),
+    });
+    reader.split('\n' as u8)
+        .map(|b| b.unwrap())
+        .map(String::from_utf8)
+        .map(|s| Ok(s.unwrap()))
 }
 
-fn main() {
+fn main() -> io::Result<()> {
     let mut result = 0;
     let mut result2 = 0;
-    for line in read_input().lines() {
-        let mass: i32 = line.parse()
-            .expect("Failed to parse integer");
-
+    for line in input() {
+        let mass: i32 = line?.parse().unwrap();
         let mut fuel = fuel_for(mass);
-
         result += fuel;
         while fuel > 0 {
             result2 += fuel;
@@ -37,4 +35,5 @@ fn main() {
 
     println!("Result: {}", result);
     println!("Result2: {}", result2);
+    Ok(())
 }
