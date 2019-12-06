59c5e9217309876ef3c2f10063ef8d234a0d71b9
aragaer <aragaer@gmail.com>
2019-12-06 04:52:56 +0300

    Day 4 much faster

--- 2019/day4/src/main.rs
+++ 2019/day4/src/main.rs
@@ -1,31 +1,33 @@
-extern crate itertools;
-
-use itertools::Itertools;
-
-
 fn main() {
     let mut result = 0;
     let mut result2 = 0;
-    for x in 136818..(685979+1) {
-        if x.to_string().chars()
-            .zip(x.to_string().chars().skip(1))
-            .any(|(f, s)| f > s) {
-                continue;
+    'outer: for mut x in 136818..(685979+1) {
+        let mut last = 10;
+        let mut group_size = 1;
+        let mut have_group = false;
+        let mut have_group_2 = false;
+        while x > 0 {
+            let digit = x % 10;
+            if digit > last {
+                continue 'outer;
             }
+            if digit == last {
+                group_size += 1;
+            } else {
+                last = digit;
+                have_group |= group_size > 1;
+                have_group_2 |= group_size == 2;
+                group_size = 1;
+            }
+            x /= 10;
+        }
+        have_group |= group_size > 1;
+        have_group_2 |= group_size == 2;
 
-        let mut have_same = false;
-        let mut have_same_non_grouped = false;
-        for count in x.to_string().chars()
-            .group_by(|x| *x)
-            .into_iter()
-            .map(|(_, g)| g.count()) {
-                have_same |= count > 1;
-                have_same_non_grouped |= count == 2;
-            }
-        if have_same {
+        if have_group {
             result += 1;
         }
-        if have_same_non_grouped {
+        if have_group_2 {
             result2 += 1;
         }
     }
