11416fcaa95672df758161ab8258b6e78b7d1264
aragaer <aragaer@gmail.com>
2020-12-05 11:17:29 +0300

    2020 05 refactor

--- 2020/05.hs
+++ 2020/05.hs
@@ -1,16 +1,15 @@
 import Data.List
+import Numeric
 
 main = getContents >>= solve
-
-passToId s = f 0 s
-  where
-    f r "" = r
-    f r (x:xs) = f (r*2 + if x `elem` "BR" then 1 else 0) xs
 
 findMissing ids = searchIn $ zip ids (tail ids)
   where
     searchIn [] = error "Nothing left"
     searchIn ((a,b):xs) = if a - b == 2 then a - 1 else searchIn xs
+
+passToId = fst . head . readInt 2 (`elem` "FBRL") f
+  where f = fromEnum . (`elem` "BR")
 
 solve dat = do
   let ids = reverse $ sort $ map passToId $ lines dat
