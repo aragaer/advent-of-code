a9938a61b57f5d152eb5aef9f373b725b6021a4e
aragaer <aragaer@gmail.com>
2020-12-06 13:19:04 +0300

    2020 06 rewrite using sets

--- 2020/06.hs
+++ 2020/06.hs
@@ -1,13 +1,15 @@
+import qualified Data.Set as Set
+
 main = getContents >>= solve
 
 toGroups = go [] . lines
   where
     go acc [] = [acc]
     go acc ("":xs) = acc:go [] xs
-    go acc (x:xs) = go (x:acc) xs
+    go acc (x:xs) = go (Set.fromList x:acc) xs
 
-countAnswersAny group = length $ filter (`elem` unwords group) ['a'..'z']
-countAnswersAll group = length $ filter (\c -> all (c `elem`) group) ['a'..'z']
+countAnswersAny = Set.size . foldl1 Set.union
+countAnswersAll = Set.size . foldl1 Set.intersection
 
 solve dat = do
   let groups = toGroups dat
