73c779eed49a5abbed2c3045186dadb7346a33e1
aragaer <aragaer@gmail.com>
2021-12-08 01:41:17 +0300

    Optimize 2021 day 6

--- 2021/06.clj
+++ 2021/06.clj
@@ -1,17 +1,8 @@
 #!/usr/bin/env bb
 
-(let [count-by-age (frequencies (map edn/read-string (str/split (read-line) #",")))]
+(let [count-by-age (frequencies (map edn/read-string (str/split (read-line) #",")))
+      counts (map #(get count-by-age % 0) (range 9))
+      its (iterate (fn [[b0 b1 b2 b3 b4 b5 b6 b7 b8]]
+                     (list b1 b2 b3 b4 b5 b6 (+ b7 b0) b8 b0)) counts)]
   (doseq [days [80 256]]
-    (println
-     (loop [this-count count-by-age
-            days days]
-       (if (= 0 days)
-         (reduce + (vals this-count))
-         (recur
-          (merge-with +
-                      (reduce (fn [counts age]
-                                (assoc counts age (get this-count (inc age) 0)))
-                              {} (range 9))
-                      {6 (get this-count 0 0)
-                       8 (get this-count 0 0)})
-          (dec days)))))))
+    (println (reduce + (nth its days)))))
