dd6ecb8cd946e2f5f90fda6ea4188dea98b51ab7
aragaer <aragaer@gmail.com>
2022-12-22 19:10:20 +0300

    2022 day 22 -- matrix operations

--- 2022/day22.erl
+++ 2022/day22.erl
@@ -2,6 +2,14 @@
 
 -module(day22).
 -mode(compile).
+-define(UP, {0,-1}).
+-define(DOWN, {0,1}).
+-define(LEFT, {-1,0}).
+-define(RIGHT, {1,0}).
+-define(T_0, {1,0,0,1}).
+-define(T_CW, {0,-1,1,0}).
+-define(T_CCW, {0,1,-1,0}).
+-define(T_180, {-1,0,0,-1}).
 
 parse_map(Lines) ->
     lists:foldl(fun ({Y,L},D) ->
@@ -10,33 +18,30 @@ parse_map(Lines) ->
                                     end, D, lists:enumerate(L))
                 end, maps:new(), lists:enumerate(Lines)).
 
-dir2num(right) -> 0;
-dir2num(down) -> 1;
-dir2num(left) -> 2;
-dir2num(up) -> 3.
+dir2num(?RIGHT) -> 0;
+dir2num(?DOWN) -> 1;
+dir2num(?LEFT) -> 2;
+dir2num(?UP) -> 3.
 
-move({X,Y},right) -> {X+1,Y};
-move({X,Y},left) -> {X-1,Y};
-move({X,Y},up) -> {X,Y-1};
-move({X,Y},down) -> {X,Y+1}.
+move({X,Y},{DX,DY}) -> {X+DX,Y+DY}.
+turn({X,Y},{Axx,Axy,Ayx,Ayy}) -> {X*Axx+Y*Axy,X*Ayx+Y*Ayy}.
+rev({Axx,Axy,Ayx,Ayy}) -> {Axx,Ayx,Axy,Ayy}.
+back(D) -> list_to_tuple([-X || X <- tuple_to_list(D)]).
+turn_where({A,B},{C,D}) -> {A*C+B*D,C*B-A*D,A*D-C*B,A*C+B*D}.
+combine({A,B,C,D},{E,F,G,H}) -> {A*E+B*G,A*F+B*H,C*E+D*G,C*F+D*H}.
 
-rot({X,Y},0) -> {X,Y};
-rot({X,Y},$L) -> {Y,1-X};
-rot({X,Y},$R) -> {1-Y,X};
-rot({X,Y},180) -> {1-X,1-Y}.
-
-bound(V,Mod) -> ((V rem Mod) + Mod - 1) rem Mod + 1.
+bound(V,Mod) -> ((round(V) rem Mod) + Mod - 1) rem Mod + 1.
 
 look(Map,{X,Y},Dir,{Size,Wrap}) ->
     {SX,SY} = {(X-1) div Size, (Y-1) div Size},
     {OBX,OBY} = {SX*Size, SY*Size},
     {OX,OY} = {X-OBX,Y-OBY},
     {NOX,NOY} = move({OX,OY},Dir),
-    {NC,T} = if NOX > 0, NOX =< Size, NOY > 0, NOY =< Size -> {{OBX+NOX,OBY+NOY},0};
+    {NC,T} = if NOX > 0, NOX =< Size, NOY > 0, NOY =< Size -> {{OBX+NOX,OBY+NOY},?T_0};
                 true ->
                      {{NSX,NSY},NT} = maps:get({{SX,SY},Dir},Wrap),
-                     {AX,AY} = rot({NOX,NOY},NT),
-                     {TX,TY} = {bound(AX,Size), bound(AY,Size)},
+                     {AX,AY} = turn({NOX-0.5,NOY-0.5},NT),
+                     {TX,TY} = {bound(AX+0.5,Size), bound(AY+0.5,Size)},
                      {{NSX*Size+TX,NSY*Size+TY},NT}
              end,
     {NC,maps:get(NC,Map),T}.
@@ -48,26 +53,6 @@ try_move(Map,C,Dir,N,Ctx) ->
        true -> try_move(Map,NC,turn(Dir,Turn),N-1,Ctx)
     end.
 
-turn(up,0) -> up;
-turn(down,0) -> down;
-turn(left,0) -> left;
-turn(right,0) -> right;
-turn(right,$R) -> down;
-turn(right,$L) -> up;
-turn(left,$R) -> up;
-turn(left,$L) -> down;
-turn(up,$R) -> right;
-turn(up,$L) -> left;
-turn(down,$R) -> left;
-turn(down,$L) -> right;
-turn(right,180) -> left;
-turn(left,180) -> right;
-turn(up,180) -> down;
-turn(down,180) -> up.
-
-turn_where(D1,D2) -> hd(lists:filtermap(fun (R) -> turn(D1,R) == D2 end,
-                                        [0,$L,$R,180])).
-
 navigate(Map,Is,C,Dir,Ctx) ->
     {ok, [N], RIs} = io_lib:fread("~u", Is),
     {NC,ND} = try_move(Map,C,Dir,N,Ctx),
@@ -76,7 +61,11 @@ navigate(Map,Is,C,Dir,Ctx) ->
             {X,Y} = NC,
             Y * 1000 + X * 4 + dir2num(ND);
        true ->
-            NDir = turn(ND,hd(RIs)),
+            T = case hd(RIs) of
+                    $L -> ?T_CCW;
+                    $R -> ?T_CW
+                end,
+            NDir = turn(ND,T),
             %io:format("Looking ~p now~n", [NDir]),
             navigate(Map,tl(RIs),NC,NDir,Ctx)
     end.
@@ -89,14 +78,14 @@ find_neighbours({X,Y},Sides) ->
                                      true -> {true, {D, NC}};
                                      _ -> false
                                  end
-                         end, [up,left,down,right]),
+                         end, [?UP,?LEFT,?DOWN,?RIGHT]),
     %io:format("~p: ~p~n", [Side, Ns]),
     lists:map(fun ({D, N}) ->
                       {F,T} = s2k({X,Y},N),
                       ND = if F == {X,Y} -> D;
-                              true -> turn(D, 180)
+                              true -> turn(D, ?T_180)
                            end,
-                      {{F,T},{ND,0}}
+                      {{F,T},{ND,?T_0}}
               end, Ns).
 
 known_neighs(S,Edges,Sides) ->
@@ -105,18 +94,6 @@ known_neighs(S,Edges,Sides) ->
                          not lists:member(Rel, [unknown,opposite]);
                      (_) -> false
                  end, Sides).
-
-rev($L) -> $R;
-rev($R) -> $L;
-rev(0) -> 0;
-rev(180) -> 180.
-
-back($L) -> $R;
-back($R) -> $L;
-back(0) -> 180;
-back(180) -> 0.
-
-combine(T1,T2) -> turn_where(up,turn(turn(up,T1),T2)).
 
 edge_dir(S1,S2,Edges) ->
     K = s2k(S1,S2),
@@ -143,7 +120,7 @@ glue_sides(Edges,Sides) ->
                               D = turn(D1,combine(T1,turn_where(D1,D2))),
                               T = combine(T2,combine(rev(T1),turn_where(D1,D2))),
                               case turn_where(D1,D2) of
-                                  180 -> maps:put({S1,S2},opposite,Es);
+                                  ?T_180 -> maps:put({S1,S2},opposite,Es);
                                   _ -> maps:put({S1,S2},{D,T},Es)
                               end
                       end, Edges, CanGlue),
@@ -169,7 +146,7 @@ wrap(C,Dir,Sides) ->
     NC = move(C,Dir),
     case lists:member(NC, Sides) of
         true -> NC;
-        _ -> go_back(C,turn(Dir,180),Sides)
+        _ -> go_back(C,turn(Dir,?T_180),Sides)
     end.
 
 main(_) ->
@@ -185,12 +162,12 @@ main(_) ->
     Start = list_to_tuple([X * Size+1 || X <- tuple_to_list(hd(Sides))]),
     Edges = maps:from_keys([{F,T} || F <- Sides, T <- Sides, F < T], unknown),
     NEdges = make_cube(Sides,Edges),
-    Wrapped = maps:from_list([{{S,D},{wrap(S,D,Sides),0}}
-                              || S <- Sides, D <- [up,left,down,right]]),
+    Wrapped = maps:from_list([{{S,D},{wrap(S,D,Sides),?T_0}}
+                              || S <- Sides, D <- [?UP,?LEFT,?DOWN,?RIGHT]]),
     Glued = maps:from_list(lists:flatmap(fun ({_,opposite}) -> [];
                                              ({{S1,S2},{D,T}}) ->
                                                  [{{S1,D},{S2,T}},
                                                   {{S2,turn(D,back(T))},{S1,rev(T)}}]
                                          end, maps:to_list(NEdges))),
-    io:format("~p~n~p~n", [navigate(Map,Instructions,Start,right,{Size,Wrapped}),
-                           navigate(Map,Instructions,Start,right,{Size,Glued})]).
+    io:format("~p~n~p~n", [navigate(Map,Instructions,Start,?RIGHT,{Size,Wrapped}),
+                           navigate(Map,Instructions,Start,?RIGHT,{Size,Glued})]).
