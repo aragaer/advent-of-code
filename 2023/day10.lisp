12a3e08210744105a9b05ae82403e67425d3c0ac
aragaer <aragaer@gmail.com>
2023-12-10 20:29:36 +0300

    2023 day 10

---
+++ 2023/day10.lisp
@@ -0,0 +1,93 @@
+#!/usr/bin/env -S sbcl --noinform --script
+
+#-quicklisp
+(let ((quicklisp-init (merge-pathnames "quicklisp/setup.lisp" (user-homedir-pathname))))
+  (when (probe-file quicklisp-init)
+    (load quicklisp-init)))
+
+(ql:quickload :str :silent t)
+
+(defvar *nodes* (make-hash-table :test #'equal))
+(defvar *conns* '((#\| :n :s)
+                  (#\- :w :e)
+                  (#\L :n :e)
+                  (#\J :n :w)
+                  (#\7 :w :s)
+                  (#\F :e :s)
+                  (#\.)
+                  (#\S :n :s :e :w)))
+(defvar *states* (make-hash-table :test #'equal))
+(defvar *start*)
+
+(defun move (location direction)
+  (destructuring-bind (y . x) location
+    (ecase direction
+      (:n (cons (- y 1) x))
+      (:e (cons y (+ x 1)))
+      (:w (cons y (- x 1)))
+      (:s (cons (+ y 1) x)))))
+
+(defun flip (direction)
+  (ecase direction
+    (:n :s)
+    (:e :w)
+    (:s :n)
+    (:w :e)
+    (:inside :outside)
+    (:outside :inside)))
+
+(defvar *size-x*)
+(defvar *size-y*)
+
+(loop for line = (read-line *standard-input* nil nil)
+      for y from 0
+      while line
+      do (setf *size-x* (length line))
+      do (loop for c across line
+               for x from 0
+               for key = (cons y x)
+               do (setf (gethash key *nodes*) (cdr (assoc c *conns*)))
+               if (eq c #\S)
+                 do (setf *start* key))
+      finally (setf *size-y* y))
+
+(setf (gethash *start* *nodes*)
+      (remove-if-not
+       (lambda (direction)
+         (let ((node (gethash (move *start* direction) *nodes*))
+               (need (flip direction)))
+           (member need node)))
+       '(:n :s :e :w)))
+
+(loop for steps from 1
+      for location = *start* then new-location
+      for come-from = (flip (car (gethash *start* *nodes*))) then (flip go-to)
+      for go-to = (find-if-not (lambda (x) (eq x come-from))
+                               (gethash location *nodes*))
+      for new-location = (move location go-to)
+      do (setf (gethash new-location *states*) :loop)
+      until (equal new-location *start*)
+      finally (format t "~a~%" (/ steps 2)))
+
+(dotimes (y *size-y*)
+  (let ((state :outside)
+        loop-came-from)
+    (dotimes (x *size-x*)
+      (let ((pos (cons y x)))
+        (if (eq :loop (gethash pos *states*))
+            (let ((loop-ends (intersection '(:n :s) (gethash pos *nodes*))))
+              (cond
+                ((and loop-came-from (member loop-came-from loop-ends))
+                 (setf loop-came-from nil))
+                ((and loop-came-from loop-ends)
+                 (setf state (flip state)
+                       loop-came-from nil))
+                ((= 2 (length loop-ends))
+                 (setf state (flip state)))
+                (loop-ends
+                 (setf loop-came-from (car loop-ends)))))
+            (setf (gethash pos *states*) state))))))
+
+(loop for v being the hash-values of *states*
+      count (eq v :inside) into result
+      finally (format t "~a~%" result))
