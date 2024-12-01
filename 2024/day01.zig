84c1eaa0fbdcc616e769c511cf3667701fb1590b
aragaer <aragaer@gmail.com>
2024-12-01 15:42:45 +0300

    2024 day 01 cleanup

--- 2024/day01.zig
+++ 2024/day01.zig
@@ -2,21 +2,19 @@ const std = @import("std");
 
 const info = std.debug.print;
 const Allocator = std.mem.Allocator;
-const List = std.ArrayList(u32);
+const List = std.ArrayList(i32);
+const Parser = std.fmt.parseInt;
 
 fn get_input(allocator: Allocator) !struct { first: List, second: List } {
     const reader = std.io.getStdIn().reader();
     var buffer: [100]u8 = undefined;
-    var first = List.init(allocator);
-    var second = List.init(allocator);
+    var result = .{ .first = List.init(allocator), .second = List.init(allocator) };
     while (try reader.readUntilDelimiterOrEof(&buffer, '\n')) |line| {
-        var splits = std.mem.tokenizeAny(u8, line, " \t");
-        const f = try std.fmt.parseInt(u32, splits.next().?, 10);
-        const s = try std.fmt.parseInt(u32, splits.next().?, 10);
-        try first.append(f);
-        try second.append(s);
+        var words = std.mem.tokenizeAny(u8, line, " \t");
+        try result.first.append(try Parser(i32, words.next().?, 10));
+        try result.second.append(try Parser(i32, words.next().?, 10));
     }
-    return .{ .first = first, .second = second };
+    return result;
 }
 
 pub fn main() !void {
@@ -28,25 +26,29 @@ pub fn main() !void {
     defer data.first.deinit();
     defer data.second.deinit();
 
-    std.mem.sort(u32, data.first.items, {}, comptime std.sort.asc(u32));
-    std.mem.sort(u32, data.second.items, {}, comptime std.sort.asc(u32));
+    std.mem.sort(i32, data.first.items, {}, comptime std.sort.asc(i32));
+    std.mem.sort(i32, data.second.items, {}, comptime std.sort.asc(i32));
 
-    var secondMap = std.AutoHashMap(u32, u32).init(allocator);
+    var firstMap = std.AutoHashMap(i32, i32).init(allocator);
+    var secondMap = std.AutoHashMap(i32, i32).init(allocator);
+    defer firstMap.deinit();
     defer secondMap.deinit();
 
     var part1: u32 = 0;
     for (data.first.items, data.second.items) |f, s| {
-        part1 += if (f > s) f - s else s - f;
-        if (secondMap.get(s)) |cnt| {
-            try secondMap.put(s, cnt + 1);
-        } else {
-            try secondMap.putNoClobber(s, 1);
-        }
+        part1 += @abs(f - s);
+        const fv = try firstMap.getOrPut(f);
+        const sv = try secondMap.getOrPut(s);
+        fv.value_ptr.* = 1 + if (fv.found_existing) fv.value_ptr.* else 0;
+        sv.value_ptr.* = 1 + if (sv.found_existing) sv.value_ptr.* else 0;
     }
     info("{}\n", .{part1});
 
-    var part2: u32 = 0;
-    for (data.first.items) |i|
-        part2 += i * (secondMap.get(i) orelse 0);
+    var part2: i32 = 0;
+    var iter = firstMap.iterator();
+    while (iter.next()) |entry| {
+        const key = entry.key_ptr.*;
+        part2 += key * entry.value_ptr.* * (secondMap.get(key) orelse 0);
+    }
     info("{}\n", .{part2});
 }
