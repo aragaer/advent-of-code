d5c5f4d2b5f0b27376aa521770cbb8cfdb1dc88b
aragaer <aragaer@gmail.com>
2024-12-10 11:44:31 +0300

    2024 day 10 cleanup

--- 2024/day10.zig
+++ 2024/day10.zig
@@ -28,9 +28,9 @@ pub fn main() !void {
 
     var linenum: i32 = 0;
     const reader = std.io.getStdIn().reader();
-    while (try reader.readUntilDelimiterOrEofAlloc(allocator, '\n', 1024)) |buf| {
-        defer allocator.free(buf);
-        for (buf, 0..) |ch, idx| {
+    var buffer: [1024]u8 = undefined;
+    while (try reader.readUntilDelimiterOrEof(&buffer, '\n')) |slice| {
+        for (slice, 0..) |ch, idx| {
             const height = std.fmt.charToDigit(ch, 10) catch @panic("not a digit");
             try area.put(c(idx, linenum), height);
             if (height == 0)
@@ -43,41 +43,30 @@ pub fn main() !void {
 
     var visited = std.AutoHashMap(Coord, usize).init(allocator);
     defer visited.deinit();
-    var layer = std.ArrayList(Coord).init(allocator);
-    defer layer.deinit();
-    var layer2 = std.ArrayList(Coord).init(allocator);
-    defer layer2.deinit();
+    var queue = std.fifo.LinearFifo(Coord, .Dynamic).init(allocator);
+    defer queue.deinit();
     var part1: usize = 0;
     var part2: usize = 0;
     for (heads.items) |head| {
         visited.clearRetainingCapacity();
         try visited.put(head, 1);
-        layer.clearRetainingCapacity();
-        try layer.append(head);
-
-        inline for (0..9) |height| {
-            layer2.clearRetainingCapacity();
-            for (layer.items) |location|
-                inline for (directions) |direction| {
-                    const next = location.add(direction);
-                    if (area.get(next) == height + 1) {
-                        const seen = visited.get(next) orelse 0;
-                        if (seen == 0)
-                            try layer2.append(next);
-                        try visited.put(next, seen + visited.get(location).?);
-                    }
-                };
-            const tmp = layer;
-            layer = layer2;
-            layer2 = tmp;
+        try queue.writeItem(head);
+        while (queue.readItem()) |location| {
+            const height = area.get(location) orelse @panic("outside");
+            inline for (directions) |direction| {
+                const next = location.add(direction);
+                if (area.get(next) == height + 1) {
+                    const seen = visited.get(next) orelse 0;
+                    if (seen == 0 and height < 8)
+                        try queue.writeItem(next);
+                    try visited.put(next, seen + visited.get(location).?);
+                }
+            }
         }
-
-        for (tails.items) |tail|
-            if (visited.get(tail)) |visits| {
-                part1 += 1;
-                part2 += visits;
-            };
+        for (tails.items) |tail| {
+            part1 += @intFromBool(visited.contains(tail));
+            part2 += visited.get(tail) orelse 0;
+        }
     }
-
     info("{}\n{}\n", .{ part1, part2 });
 }
