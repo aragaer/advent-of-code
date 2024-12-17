c51abb270eb40493873386c622f705174c9328f4
aragaer <aragaer@gmail.com>
2024-12-17 13:06:49 +0300

    2024 day 17 cleanup

--- 2024/day17.zig
+++ 2024/day17.zig
@@ -5,43 +5,36 @@ const Reg = u64;
 var regs = [_]Reg{ 0, 0, 0 };
 var ptr: usize = 0;
 var insns: std.ArrayList(u3) = undefined;
-var output: std.ArrayList(u3) = undefined;
 
 inline fn combo(value: u3) u3 {
     return switch (value) {
-        0, 1, 2, 3 => value,
-        4, 5, 6 => @as(u3, @intCast(regs[value - 4] & 7)),
+        0...3 => value,
+        4...6 => @as(u3, @intCast(regs[value - 4] & 7)),
         else => @panic("wrong combo operand"),
     };
 }
 
-fn step() !void {
+fn step() ?u3 {
     const operand = insns.items[ptr + 1];
     var nptr: ?usize = null;
+    defer ptr = if (nptr) |n| n else ptr + 2;
     switch (insns.items[ptr]) {
         0 => regs[0] = regs[0] >> combo(operand),
         1 => regs[1] ^= operand,
         2 => regs[1] = combo(operand),
         3 => nptr = if (regs[0] == 0) null else operand,
         4 => regs[1] ^= regs[2],
-        5 => output.append(combo(operand)) catch @panic("out"),
+        5 => return combo(operand),
         6 => regs[1] = regs[0] >> combo(operand),
         7 => regs[2] = regs[0] >> combo(operand),
     }
-    ptr = if (nptr) |n| n else ptr + 2;
+    return null;
 }
 
-fn run(a: Reg) !void {
-    regs[0] = a;
-    ptr = 0;
-    output.clearRetainingCapacity();
+fn run() ?u3 {
     while (ptr < insns.items.len)
-        try step();
-}
-
-const I = struct { offt: usize, value: Reg };
-fn item_cmp(_: void, a: I, b: I) bool {
-    return a.value > b.value;
+        return step() orelse continue;
+    return null;
 }
 
 pub fn main() !void {
@@ -56,15 +49,13 @@ pub fn main() !void {
         var pv = std.mem.tokenizeScalar(u8, line, ' ');
         _ = pv.next();
         _ = pv.next();
-        regs[idx] = try std.fmt.parseInt(Reg, pv.next().?, 10);
+        regs[idx] = try std.fmt.parseInt(Reg, pv.rest(), 10);
     }
 
     _ = try reader.readUntilDelimiterOrEof(&buf, '\n');
 
     insns = @TypeOf(insns).init(allocator);
     defer insns.deinit();
-    output = @TypeOf(output).init(allocator);
-    defer output.deinit();
 
     const line = (try reader.readUntilDelimiterOrEof(&buf, '\n')).?;
     var pi = std.mem.tokenizeAny(u8, line, ", ");
@@ -72,26 +63,28 @@ pub fn main() !void {
     while (pi.next()) |ins|
         try insns.append(try std.fmt.parseInt(u3, ins, 10));
 
-    try run(regs[0]);
-    for (output.items, 1..) |out, idx|
-        info("{}{c}", .{ out, ",\n"[@intFromBool(idx == output.items.len)] });
+    info("{?}", .{run()});
+    while (run()) |out|
+        info(",{}", .{out});
+    info("\n", .{});
 
-    var queue = std.ArrayList(I).init(allocator);
+    var queue = std.ArrayList(Reg).init(allocator);
     defer queue.deinit();
+    var next = std.ArrayList(Reg).init(allocator);
+    defer next.deinit();
 
-    try queue.append(I{ .offt = insns.items.len - 1, .value = 0 });
-    while (queue.popOrNull()) |i| {
-        for (0..8) |v| {
-            const nv = i.value << 3 | v;
-            try run(nv);
-            if (std.mem.eql(u3, insns.items[i.offt..], output.items)) {
-                if (i.offt == 0) {
-                    info("{}\n", .{nv});
-                    return;
-                }
-                try queue.append(I{ .offt = i.offt - 1, .value = nv });
-            }
-        }
-        std.mem.sort(I, queue.items, {}, item_cmp);
+    try queue.append(0);
+    for (0..insns.items.len) |offt| {
+        while (queue.popOrNull()) |i|
+            inline for (0..8) |v| {
+                const nv = i << 3 | v;
+                regs[0] = nv;
+                ptr = 0;
+                if (run() == insns.items[insns.items.len - offt - 1])
+                    try next.append(nv);
+            };
+        try queue.insertSlice(0, next.items);
+        next.clearRetainingCapacity();
     }
+    info("{}\n", .{queue.items[0]});
 }
